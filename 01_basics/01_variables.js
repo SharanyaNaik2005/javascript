bcac2ea69c7b36aec5f9183d90fd69f238a8668c
SharanyaNaik2005 <sharanyanaik2005@gmail.com>
2025-12-29 11:30:51 +0530

    Initial commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "sharanya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Udupi"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "sharu@gmail.com"
+accountPasword = "212222"
+accountCity = "udupi"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// alert(9+9)
+
+// console.log(9+9)
+let name = "sgaranya"
+let age = 20
+
+// console.log("sharanya");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined 
